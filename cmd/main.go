12dd98d8ef22b28372954e9e5c328d8d84b9fe76
Viktoria <android1986mir@rambler.ru>
2023-03-31 14:46:55 +0300

    main

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,23 @@
 package main
 
+import (
+	"fmt"
 
-import(
 	"28.1/interial/app"
 	"28.1/interial/storage"
+	sl "github.com/VictoriaNac/logg"
 )
 
 func main(){
+
+	l, err :=sl.NewLogger ("log.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(l)
+
+
+
 	storage := storage.NewStorage() //экземпляр структуры репозитория
 	app := app.NewApp(storage)      //экземпляр структуры приложения
 	app.Run()
