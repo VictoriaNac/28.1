63079adda7bd874e7cd5bec5e6de6cd2e7a37cd3
Viktoria <android1986mir@rambler.ru>
2023-04-05 10:16:50 +0300

    home

--- cmd/main.go
+++ cmd/main.go
@@ -14,17 +14,40 @@ func main(){
 		Storage:storage,
 		}                            
 	app.Run()
+	
 
+//Реализуйте методы в репозитории и вывод списка зачисленных студентов
 
+type Storage map[string]*student.Student
 
-	/*
-a := student.NewStudent("Вася", 24, 1)
-	b := student.NewStudent("Семен", 32, 2)
-	ss := storage.NewStudentStorage()
-	ss.Put(a)
-	ss.Put(b)
-	fmt.Println(ss.Get(a.Name))
-	fmt.Println(ss.Get(b.Name))
+func (NewStorage) (Storage); {
+	return make(Storage)
 }
-	*/
+
+func (s Storage) Put(value *student.Student) error {
+	s[value.GetName()] = value
+
+	if s[value.GetName()] == nil {
+		return errors.New("smt")
+	} else {
+		return nil
+	}
+}
+
+func (s Storage) Get(name string) (*student.Student, error) {
+	if s[name] == nil {
+		return nil, errors.New("smt1")
+	} else {
+		return s[name], nil
+	}
+}
+
+func (s *Storage) PrintStudents() {
+	for _, value := range *s {
+		fmt.Print(value.GetName(), " ")
+		fmt.Print(value.GetAge(), " ")
+		fmt.Print(value.GetGrade(), "\n")
+	}
+}
+
 }
